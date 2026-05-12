8715ae48c10969e77c5e91d72895fdec5cacb2a1
praveen-analyze <epraveen952@example.com>
2026-05-12 16:32:21 +0530

    fixed render deployment

--- server/server.js
+++ server/server.js
@@ -1,19 +1,64 @@
-const express=require("express")
-const mongoose=require("mongoose")
-const cors=require("cors")
-require("dotenv").config()
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const app=express()
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.json())
+const app = express();
 
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>console.log("DB Connected"))
-.catch(err=>console.log(err))
+app.use(cors({
+    origin: "*"
+}));
 
-app.use("/api/mail",require("./routes/mailRoute"))
+app.use(express.json());
 
-app.listen(5000,()=>{
-    console.log("Server Running")
+
+// Root Route
+app.get("/", (req, res) => {
+
+    res.send(
+        "Bulk Mail API Running"
+    );
+});
+
+
+// MongoDB
+mongoose.connect(
+    process.env.MONGO_URL
+)
+.then(() => {
+
+    console.log(
+        "DB Connected"
+    );
 })
+.catch((err) => {
+
+    console.log(
+        err
+    );
+});
+
+
+// Routes
+app.use(
+    "/api/mail",
+    require(
+        "./routes/mailRoute"
+    )
+);
+
+
+// Render Port
+const PORT =
+    process.env.PORT || 5000;
+
+app.listen(
+    PORT,
+    () => {
+
+        console.log(
+            "Server Running"
+        );
+    }
+);
