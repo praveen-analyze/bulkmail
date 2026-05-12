06798a4f01fa85431ade0506bbe35b007cf037f1
praveen-analyze <epraveen952@example.com>
2026-05-12 16:50:45 +0530

    fixed smtp render issue

--- server/routes/mailRoute.js
+++ server/routes/mailRoute.js
@@ -3,7 +3,15 @@ const nodemailer = require("nodemailer");
 const Mail = require("../models/MailModel");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+
+    host: "smtp.gmail.com",
+
+    port: 587,
+
+    secure: false,
+
+    family: 4,
+
     auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD
